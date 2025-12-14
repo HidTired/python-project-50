c141cbd4a379baea5a05b351bf1cc570ea9b9de4
HidTired <Dentanyabonya@mail.ru>
2025-12-14 15:33:32 +0300

    Quest

--- gendiff/utils.py
+++ gendiff/utils.py
@@ -1,16 +1,7 @@
 import json
 
 def load_data(file_path: str) -> dict | None:
-    """
-    Загружает данные из JSON-файла, с дополнительным контролем булевых значений.
-
-    :param file_path: Путь к файлу
-    :return: Словарь с данными или None в случае ошибки
-    """
     def convert_boolean(dct):
-        """
-        Хук-преобразователь, позволяющий поменять булевые значения на строки.
-        """
         converted_dct = {}
         for key, value in dct.items():
             if isinstance(value, bool):
@@ -21,9 +12,8 @@ def load_data(file_path: str) -> dict | None:
 
     try:
         with open(file_path, 'r') as f:
-            # Читаем данные с преобразованием булевых значений
             data = json.load(f, object_hook=convert_boolean)
-            print("Данные после загрузки:", data)  # Diagnostic output
+            print("Данные после загрузки:", data) 
             return data
     except Exception as e:
         print(f"Ошибка при чтении файла {file_path}: {e}")
